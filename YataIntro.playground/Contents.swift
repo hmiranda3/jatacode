507652243d9b52ac4cc960bab2e381c18a315c76
Habib Miranda Garcia <>
2018-12-15 09:30:07 -0800

    Intro Playground and Swift Basics

---
+++ YataIntro.playground/Contents.swift
@@ -0,0 +1,36 @@
+//Import Statements:
+import UIKit
+
+//Declaring a variable:
+/* string, Int, Double, Float, Boolean */
+
+
+
+//Create two arrays. One of type string, and one of type int:
+
+
+
+//Declaring a constant: (Swift)
+
+
+
+//Declaring a function:
+
+
+
+//Creating a class:
+
+
+
+//Creating an Enumeration:
+
+
+
+//Operators:
+
+    /* Write a method that adds two variables together: */
+
+
+
+    /* Write a method that returns all items in an array that are even numbers: */
+
